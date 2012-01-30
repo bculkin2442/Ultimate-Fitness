972a3760c5de4ac3b2b4eec6772bab5a5b1e6349
Benjamin Culkin <scorpress@gmail.com>
2012-01-30 10:11:00 -0500

    index.php changes

--- ultimate/index.php
+++ ultimate/index.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 			<title>Ultimate Fitness Home</title>
+            <link rel="stylesheet" type="text/css" href="/ultimate/templates/assets/css/def.css"/>
 	</head>
 	<body>
 		
